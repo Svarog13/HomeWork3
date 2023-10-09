255906779a9a2713db0a8e18e33fc4d29d7845be
lenovo <lenovo@DESKTOP-D0T6Q1U>
2023-10-09 22:23:04 +0300

    Add project files.

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter four digit: ");
+
+int result = 0;
+for (int i = 0; i < 4; i++)
+{
+    string input = Console.ReadLine();
+    if (int.TryParse(input, out int digit) && input.Length == 1)
+    {
+        result = result * 10 + digit;
+    }
+    else
+    {
+        Console.WriteLine("Wrong enter. Please, ente one digit.");
+        i--;
+    }
+}
+
+Console.WriteLine($"We have digit: {result}");
